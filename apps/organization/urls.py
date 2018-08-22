36986bb37829ef345d65fdb041b3cc320b1b99e3
wj <wj@magedu.com>
2018-08-22 23:41:27 +0800

    完善机构内的相关页面

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls import url
 
-from organization.views import OrgView, AddUserAskView
+from organization.views import OrgView, AddUserAskView, OrgHomeView, OrgCourseView, OrgDescView, OrgTeacherView
 
 
 app_name = 'org'
 
 urlpatterns = [
     url(r"^list/$", OrgView.as_view(), name="org_list"),  # 课程机构页面
-    url(r'^add_ask/', AddUserAskView.as_view(), name="add_ask")  # 我要学习提交表单
+    url(r'^add_ask/', AddUserAskView.as_view(), name="add_ask"),  # 我要学习提交表单
+    url(r'^home/(?P<org_id>\d+)/$', OrgHomeView.as_view(), name="org_home"),  # 机构首页
+    url(r'^course/(?P<org_id>\d+)/$', OrgCourseView.as_view(), name="org_course"),  # 机构课程
+    url(r'^desc/(?P<org_id>\d+)/$', OrgDescView.as_view(), name="org_desc"),  # 机构介绍
+    url(r'^teacher/(?P<org_id>\d+)/$', OrgTeacherView.as_view(), name="org_teacher"),  # 机构介绍
+
 ]
--- apps/organization/views.py
+++ apps/organization/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 
 from .models import CourseOrg, CityDict
 from .forms import UserAskForm
+from course.models import Course
 
 
 # Create your views here.
@@ -75,3 +76,63 @@ class AddUserAskView(View):
             return HttpResponse('{"status": "success"}', content_type='application/json')
         else:
             return HttpResponse('{"status": "fail", "msg": "添加出错"}', content_type='application/json')
+
+
+# 机构首页
+class OrgHomeView(View):
+    def get(self, request, org_id):
+        course_org = CourseOrg.objects.get(id=int(org_id))
+        all_courses = course_org.course_set.all()[:3]  # 取出三门课程
+        all_teacher = course_org.teacher_set.all()[:1]  # 取出一个教师
+
+        current_page = "home"
+
+        return render(request, 'org-detail-homepage.html', {
+            'all_courses': all_courses,
+            'all_teacher': all_teacher,
+            'course_org': course_org,
+            'current_page': current_page
+        })
+
+
+# 机构课程列表页
+class OrgCourseView(View):
+    def get(self, request, org_id):
+        course_org = CourseOrg.objects.get(id=int(org_id))  # 课程机构
+        all_courses = course_org.course_set.all()  # 课程
+
+        current_page = "course"
+
+        return render(request, 'org-detail-course.html', {
+            'all_courses': all_courses,
+            'course_org': course_org,
+            "current_page": current_page
+        })
+
+
+# 机构介绍页
+class OrgDescView(View):
+    def get(self, request, org_id):
+        course_org = CourseOrg.objects.get(id=int(org_id))  # 课程机构
+
+        current_page = "desc"
+
+        return render(request, 'org-detail-desc.html', {
+            'course_org': course_org,
+            "current_page": current_page
+        })
+
+
+# 机构讲师
+class OrgTeacherView(View):
+    def get(self, request, org_id):
+        course_org = CourseOrg.objects.get(id=int(org_id))  # 课程机构
+        all_teachers = course_org.teacher_set.all()  # 讲师
+
+        current_page = "teacher"
+
+        return render(request, 'org-detail-teachers.html', {
+            'all_teachers': all_teachers,
+            'course_org': course_org,
+            "current_page": current_page
+        })
